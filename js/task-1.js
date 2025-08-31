23116cc42447e7c76d3a64a29d07759598164ad3
Oleh Los <oleh.los@gmail.com>
2025-08-31 17:47:59 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function slugify(title) {
+  let arr1;
+  let arr;
+  arr1 = title.toLowerCase();
+  arr = arr1.split(' ');
+  return arr.join('-');
+}
+
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
